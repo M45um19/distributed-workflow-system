02734a9ad98b0f5c224c6432d5dd25e160907f6b
Naim Istiak Masum <naimistiaknaim@gmail.com>
2026-05-12 19:36:54 +0600

    refactor(workspace): move kafka to worker and update error response structure

---
+++ services/workspace-service/cmd/worker/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/M45um19/distributed-workflow-system/services/workspace-service/config"
+	"github.com/M45um19/distributed-workflow-system/services/workspace-service/internal/app"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := config.ConnectDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rdb := config.ConnectRedis(cfg.RedisURI)
+
+	container := app.NewContainer(cfg, db, rdb, nil)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	log.Println("Workspace Background Worker is running...")
+
+	go container.KafkaWorker.Start(ctx)
+
+	<-ctx.Done()
+	log.Println("Shutting down workers gracefully...")
+}
--- services/workspace-service/pkg/apperror/error.go
+++ services/workspace-service/pkg/apperror/error.go
@@ -1,11 +1,15 @@
 package apperror
 
-import "net/http"
+import (
+	"net/http"
+	"runtime/debug"
+)
 
 type AppError struct {
 	Success    bool   `json:"success"`
 	StatusCode int    `json:"-"`
 	Message    string `json:"message"`
+	Stack      string `json:"-"`
 }
 
 func (e *AppError) Error() string {
@@ -14,17 +18,32 @@ func (e *AppError) Error() string {
 
 func NewAppError(message string, statusCode int) *AppError {
 	return &AppError{
-
 		Success:    false,
 		StatusCode: statusCode,
 		Message:    message,
+		Stack:      string(debug.Stack()),
 	}
 }
 
+// BadRequest helper
 func BadRequest(msg string) *AppError {
 	return NewAppError(msg, http.StatusBadRequest)
 }
 
+// Unauthorized helper
 func Unauthorized(msg string) *AppError {
 	return NewAppError(msg, http.StatusUnauthorized)
+}
+
+func Forbidden(msg string) *AppError {
+	return NewAppError(msg, http.StatusForbidden)
+}
+
+// NotFound helper
+func NotFound(msg string) *AppError {
+	return NewAppError(msg, http.StatusNotFound)
+}
+
+func InternalServer(msg string) *AppError {
+	return NewAppError(msg, http.StatusInternalServerError)
 }
