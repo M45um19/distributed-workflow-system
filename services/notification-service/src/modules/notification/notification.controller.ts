fe2cdf2d2356cc11141c471215875600d91d983a
Naim Istiak Masum <naimistiaknaim@gmail.com>
2026-06-07 19:14:30 +0600

    feat(notification): create fetch and update notification api routes

---
+++ services/notification-service/src/modules/notification/notification.controller.ts
@@ -0,0 +1,61 @@
+import { Response } from 'express';
+
+import { AuthRequest } from '../../middleware/auth.middleware.js';
+
+import { NotificationService } from './notification.service.js';
+
+
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  public getNotifications = async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+    
+      const userId = req.user?.userId;
+
+      if (!userId) {
+        res.status(401).json({ success: false, message: 'Unauthorized access' });
+        return;
+      }
+
+      const data = await this.notificationService.getUserNotifications(userId);
+
+      res.status(200).json({
+        success: true,
+        data: data.notifications,
+        unreadCount: data.unreadCount
+      });
+    } catch (error) {
+      console.error(`[NotificationController] getNotifications Error: ${(error as Error).message}`);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  };
+
+  public markNotificationsAsRead = async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+
+      const userId = req.user?.userId;
+      const { notificationIds } = req.body as { notificationIds: string[] };
+
+      if (!userId) {
+        res.status(401).json({ success: false, message: 'Unauthorized access' });
+        return;
+      }
+
+      if (!notificationIds || !Array.isArray(notificationIds) || notificationIds.length === 0) {
+        res.status(400).json({ success: false, message: 'Invalid or empty notificationIds array' });
+        return;
+      }
+
+      await this.notificationService.updateNotificationsAsRead(userId, notificationIds);
+
+      res.status(200).json({
+        success: true,
+        message: 'Notifications successfully marked as read'
+      });
+    } catch (error) {
+      console.error(`[NotificationController] markNotificationsAsRead Error: ${(error as Error).message}`);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  };
+}
--- services/notification-service/src/modules/notification/notification.repository.ts
+++ services/notification-service/src/modules/notification/notification.repository.ts
@@ -1,9 +1,47 @@
-import { INotification, INotificationRepository } from './notification.interface.js';
+import { 
+  IFetchNotificationsResponse, 
+  INotification, 
+  INotificationRepository 
+} from './notification.interface.js';
 import { NotificationModel } from './notification.model.js';
 
 export class NotificationRepository implements INotificationRepository {
+
   public async create(data: INotification): Promise<INotification> {
     const created = await NotificationModel.create(data);
     return created.toJSON() as INotification;
+  }
+
+  public async fetchLatest(userId: string, limit: number): Promise<IFetchNotificationsResponse> {
+    const finalLimit = limit || 20;
+    const [notifications, unreadCount] = await Promise.all([
+      NotificationModel
+        .find({ userId })
+        .sort({ createdAt: -1 })
+        .limit(finalLimit)
+        .exec(),
+      NotificationModel
+        .countDocuments({ userId, isRead: false })
+        .exec()
+    ]);
+
+    return {
+      notifications,
+      unreadCount,
+    };
+  }
+
+  public async markAsRead(userId: string, notificationIds: string[]): Promise<void> {
+    await NotificationModel
+      .updateMany(
+        {
+          userId,
+          _id: { $in: notificationIds }
+        },
+        {
+          $set: { isRead: true }
+        }
+      )
+      .exec();
   }
 }
