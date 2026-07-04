3170967ecfe0916d4d7f49e7125c0f0bac075bc0
Naim Istiak Masum <naimistiaknaim@gmail.com>
2026-07-04 22:11:27 +0600

    feat(auth): add profile fetch and update functionality

--- services/auth-service/src/app.container.ts
+++ services/auth-service/src/app.container.ts
@@ -2,17 +2,24 @@ import { AuthMiddleware } from "./middleware/auth.middleware.js";
 import { AuthController } from "./modules/auth/auth.controller.js";
 import { IAuthService } from "./modules/auth/auth.interface.js";
 import { AuthService } from "./modules/auth/auth.service.js";
+import { UserController } from "./modules/user/user.controller.js";
+import { IUserService } from "./modules/user/user.interface.js";
 import { UserRepository } from "./modules/user/user.repository.js";
+import { UserService } from "./modules/user/user.service.js";
 
 export class AppContainer {
   public authController: AuthController;
   public authService: IAuthService;
   public authMiddleware: AuthMiddleware;
+  public userController: UserController;
+  public userService: IUserService;
 
   constructor() {
     const userRepository = new UserRepository();
     this.authService = new AuthService(userRepository); 
     this.authController = new AuthController(this.authService);
     this.authMiddleware = new AuthMiddleware(this.authService);
+    this.userService = new UserService(userRepository);
+    this.userController = new UserController(this.userService);
   }
 }
--- services/auth-service/src/modules/user/user.interface.ts
+++ services/auth-service/src/modules/user/user.interface.ts
@@ -11,4 +11,24 @@ export interface IUserRepository {
   create(data: IUser): Promise<IUserDocument>;
   findByEmail(email: string): Promise<IUserDocument | null>;
   findById(id: string): Promise<IUserDocument | null>;
+  updateById(id: string, data: Partial<IUser>): Promise<IUserDocument | null>;
+}
+
+export interface UserProfileResponse {
+  id: string;
+  full_name: string;
+  email: string;
+  role?: string | undefined;
+  avatar_url: string;
+  address?: string | undefined;
+  phone?: string | undefined;
+  bio?: string | undefined;
+  city?: string | undefined;
+  country?: string | undefined;
+  created_at?: Date | undefined;
+}
+
+export interface IUserService {
+  getUserProfile(userId: string): Promise<UserProfileResponse>;
+  updateUserProfile(userId: string, data: Partial<IUser>): Promise<UserProfileResponse>;
 }
